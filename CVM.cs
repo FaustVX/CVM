cb54d9f798102c19c872164ce054d4b3ca6c22f4
FaustVX <jodu035@gmail.com>
2024-05-17 22:23:12 +0200

    Removed Round from `Clean`

--- CVM.cs
+++ CVM.cs
@@ -10,29 +10,37 @@ public partial class CVM<T>([Field]int memory)
 
     private bool IsFull => _array.Count >= _memory;
 
+    public int Count => _array.Count * (int)Math.Pow(2, _round);
+
     public void Process(T data)
     {
         if (!_array.Contains(data))
+            if (GenerateBool(_round, Rng))
             _array.AddLast(data);
         if (IsFull)
             Clean();
+
+        static bool GenerateBool(int round, Random rng)
+        => round switch
+        {
+            <= 0 => true,
+            1 => rng.NextBool(),
+            _ => rng.NextBool() && GenerateBool(round - 1, rng),
+        };
     }
 
     private void Clean()
     {
-        if (_round == 0)
+        var current = _array.Last;
+        while (current is not null)
         {
-            var current = _array.Last;
-            while (current is not null)
+            if (Rng.NextBool())
             {
-                if (Rng.NextBool())
-                {
-                    (current, var next) = (current.Previous, current!);
-                    _array.Remove(next);
-                    continue;
-                }
-                current = current.Previous;
+                (current, var next) = (current.Previous, current!);
+                _array.Remove(next);
+                continue;
             }
+            current = current.Previous;
         }
         _round++;
     }
