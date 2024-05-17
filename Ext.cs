2c3721c2b2c038482b27fbcc40517c1a40ca08c7
FaustVX <jodu035@gmail.com>
2024-05-17 23:04:11 +0200

    Added a power lookup table

--- Ext.cs
+++ Ext.cs
@@ -3,9 +3,10 @@ namespace CVM;
 
 public static class Ext
 {
+    private static Dictionary<int, int> _powers = [];
     public static bool NextBool(this Random rng)
     => rng.Next(2) == 0;
 
     public static bool NextBool(this Random rng, int power)
-    => rng.Next((int)Math.Pow(2, power)) == 0;
+    => rng.Next(_powers.TryGetValue(power, out var value) ? value : (_powers[power] = (int)Math.Pow(2, power))) == 0;
 }
